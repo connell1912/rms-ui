996d0068de83c7679d5e9a7eda3b47780249a212
ArdonPolukoff <ardonpolukoff@gmail.com>
2020-03-04 09:44:34 -0500

    Testing is started

---
+++ project3-rms/src/tests/navbar.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Enzyme from "enzyme";
+import  Adapter from 'enzyme-adapter-react-16';
+import  NavBarComponent from "../employee-service/components/display-components/NavBarComponent";
+const shallow = Enzyme.shallow;
+Enzyme.configure({adapter:new Adapter()})
+
+
+describe("NavBar Component", () => {
+ 
+  it("An experimental enzyme test", () => {
+    expect(1).toBe(1);
+  });
+ 
+  it("Should render Navbar Without Errors", () => {
+    const component = shallow(<NavBarComponent/>);
+    const wrapper = component.find(".navbarcomponent");
+    expect(wrapper.length).toBe(1);
+  });
+
+  it("Should render a logo", () => {
+    const component = shallow(<NavBarComponent/>);
+    const logo = component.find(".logoIMG");
+    expect(logo.length).toBe(1);
+  });
+});
