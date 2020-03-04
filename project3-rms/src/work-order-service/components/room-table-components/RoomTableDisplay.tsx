04197529afe6ac567e8717bae8d387b1bcfbaa45
{connorcaseyc} <{connorcaseyc@gmail.com}>
2020-03-04 13:45:09 -0500

    fixing the active status and room table

--- project3-rms/src/work-order-service/components/room-table-components/RoomTableDisplay.tsx
+++ project3-rms/src/work-order-service/components/room-table-components/RoomTableDisplay.tsx
@@ -11,11 +11,11 @@ export const RoomTableDisplay:React.FC<IRR> = (props:IRR) => {
 
         <>
             <tr key={props.room.id}>
-                <td key={props.room.id}>{props.room.id}</td>
-                {/* <td key={props.room.currentStatus}>{props.room.currentStatus}</td> */}
-                <td key={props.room.roomNumber}>{props.room.roomNumber}</td>
-                <td>{props.room.isActive}</td>
-                <td key={props.room.maxOccupancy}>{props.room.maxOccupancy}</td>
+                <td key={0}>{props.room.id}</td>
+                {/* <td key={1}>{props.room.currentStatus}</td> */}
+                <td key={1}>{props.room.roomNumber}</td>
+                <td key={2}>{props.room.isActive.valueOf().toString()}</td>
+                <td key={3}>{props.room.maxOccupancy}</td>
             </tr>
         </>
 
